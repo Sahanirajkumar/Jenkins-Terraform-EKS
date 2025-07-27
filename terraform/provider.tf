386daad11fbf4bfe2d763eb18006e33e665336a2
Sahanirajkumar <119518738+Sahanirajkumar@users.noreply.github.com>
2025-07-27 13:32:55 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,7 +2,7 @@ locals {
   region = "ap-south-1"
   name   = "raj-eks-cluster"
   vpc_cidr = "10.124.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
+  azs      = ["ap-south-1a", "ap-south-1b"]
   public_subnets  = ["10.124.1.0/24", "10.124.2.0/24"]
   private_subnets = ["10.124.3.0/24", "10.124.4.0/24"]
   intra_subnets   = ["10.124.5.0/24", "10.124.6.0/24"]
